dccd8fbdbd8295aa949120b57835049f63b438e5
Paweł <pawel@pidzikowski.ct8.pl>
2019-02-28 08:06:32 +0100

    Initial commit

---
+++ precache-manifest.101bdde62b7591e9e342534c35de0434.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "335112f0fb0e79612a67",
+    "url": "/static/js/main.335112f0.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "335112f0fb0e79612a67",
+    "url": "/static/css/main.55d611ae.chunk.css"
+  },
+  {
+    "revision": "1198e665dcd839d2e6c3dad9b9d68578",
+    "url": "/index.html"
+  }
+];
